49c2a6d30c8d6974912e46e865cf19307a94c1bf
Jose L. Gonzalez Diaz <kbcerverjo@gmail.com>
2023-01-07 00:24:50 +0000

    First Commit

--- exercises/03-What-is-a-function/app.py
+++ exercises/03-What-is-a-function/app.py
@@ -1,5 +1,11 @@
 def sum(number1,number2):
     return number1 + number2
 
-total = sum(2,3)
-print(total)
+super_duper = sum(3445324,53454423)
+print(super_duper)
+
+def multi(number1, number2):
+    return number1 * number2
+
+mipo = multi(88515, 545566)
+print(mipo)
